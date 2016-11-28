1d04894b5f8cd0e2bb034320880d3238eaa2faf5
Egor Struchkov <egor.struchkov@devtodev.com>
2016-11-28 12:32:26 +0300

    Fix

--- src/Devtodev/ApiResponse.php
+++ src/Devtodev/ApiResponse.php
@@ -1,6 +1,28 @@
 <?php
 
-class ApiResponse{
+class ApiResponse {
+    private $responseData = [];
 
-    
+    public function getResponseData() {
+        return $this->responseData;
+    }
+
+    public function setResponseData($responseData = []) {
+        $this->responseData = $responseData;
+    }
+
+    public function prepareResponse() {
+        $status = (!empty($this->responseData) && isset($this->responseData['status'])) ? $this->responseData['status'] : 0;
+        $header = (!empty($this->responseData) && isset($this->responseData['header'])) ? $this->responseData['header'] : [];
+
+        $url = "{$this->getUrl()}?api={$this->getApiKey()}";
+        echo $url;
+        echo json_encode($this->responseData);
+
+        if($status != 200) {
+            $errorMessage = (!empty($header) && isset($header['error_message'])) ? $header['error_message'] : '';
+            if(!empty($errorMessage))
+                throw new DevtodevException($errorMessage, $status);
+        }
+    }
 }
