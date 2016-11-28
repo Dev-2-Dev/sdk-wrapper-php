e6050808fd7dfc812ec680f4a76df95a5aaa00c9
Egor Struchkov <egor.struchkov@devtodev.com>
2016-11-28 12:52:27 +0300

    Fix

--- src/Devtodev/ApiResponse.php
+++ src/Devtodev/ApiResponse.php
@@ -13,12 +13,11 @@ class ApiResponse {
 
     public function prepareResponse() {
         $status = (!empty($this->responseData) && isset($this->responseData['status'])) ? $this->responseData['status'] : 0;
-        $header = (!empty($this->responseData) && isset($this->responseData['header'])) ? $this->responseData['header'] : [];
+        $header = (!empty($this->responseData) && isset($this->responseData['header'])) ? json_decode($this->responseData['header'], true) : [];
 
         if($status != 200) {
-            $errorMessage = (!empty($header) && isset($header['error_message'])) ? $header['error_message'] : '';
-            if(!empty($errorMessage))
-                throw new DevtodevException($errorMessage, $status);
+            $errorMessage = (!empty($header) && isset($header['error_message'])) ? $header['error_message'] : 'Bad response status';
+            throw new DevtodevException($errorMessage, $status);
         }
     }
 }
